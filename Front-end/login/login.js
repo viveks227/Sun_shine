8ca7d941d0d4b6f9362d4b55e122e2f66701c504
vivek227 <vieks5728@gmail.com>
2026-09-06 20:37:13 +0530

    login/form updatation

--- Front-end/login/login.js
+++ Front-end/login/login.js
@@ -1,15 +1,21 @@
-function showRegister() {
-    document.getElementById("loginForm").style.display = "none";
-    document.getElementById("registerForm").style.display = "block";
-
+// Sabhi containers ko pehle hide karo, fir sirf desired container ko show karo
+function hideAllForms() {
+    document.getElementById("loginContainer").style.display = "none";
+    document.getElementById("registerContainer").style.display = "none";
+    document.getElementById("forgetContainer").style.display = "none";
 }
 
 function showLogin() {
-    document.getElementById("loginForm").style.display = "block";
-    document.getElementById("registerForm").style.display = "none";
+    hideAllForms();
+    document.getElementById("loginContainer").style.display = "block";
+}
+
+function showRegister() {
+    hideAllForms();
+    document.getElementById("registerContainer").style.display = "block";
 }
 
 function showForget() {
-    document.getElementById("forgetForm").style.display = "block";
-    document.getElementById("loginForm").style.display = "none";
+    hideAllForms();
+    document.getElementById("forgetContainer").style.display = "block";
 }
