4aa7752a7fdc9060bbcf9c096b77b422e3c3ca47
vivek227 <vieks5728@gmail.com>
2026-09-06 21:04:07 +0530

    feature/clear-form

---
+++ Front-end/script.js
@@ -0,0 +1,6 @@
+function clearN() {
+    document.getElementById("name").value = "";
+    document.getElementById("mail").value = "";
+    document.getElementById("subject").value = "";
+    document.getElementById("mesg").value = "";
+}
